89365f9fc71eb312138a43a262933513da994390
Saidafzalkhon B. <287926457+B-Saidafzalkhon@users.noreply.github.com>
2026-06-10 14:59:16 +0500

    Add "05-animals": animal inheritance and polymorphism example

---
+++ 02-oop/05-animals/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Channels;
+
+namespace _05_animals
+{
+    internal class Program
+    {
+        public class Animal
+        {
+            public string Name { get; private set; }
+            public int Age {  get; private set; }
+
+            public Animal(string name, int age)
+            {
+                this.Name = name;
+                this.Age = age;
+            }
+
+            public virtual void MakeSound() => Console.WriteLine("Some generic sound...");
+            public void Describe() => Console.WriteLine($"Name: {Name}, Age: {Age}");
+        }
+
+        public class Dog : Animal
+        {
+            public Dog(string name, int age) : base(name, age) { }
+            public override void MakeSound() => Console.WriteLine("Woof!");
+        }
+        public class Cat : Animal
+        {
+            public Cat(string name, int age) : base(name, age) { }
+            public override void MakeSound() => Console.WriteLine("Meow!");        
+        }
+        static void Main(string[] args)
+        {
+            List<Animal> zoo = new()
+            {
+                new Dog("Wolter", 10),
+                new Cat("Kitty", 5),
+                new Dog("Hanz", 11)
+            };
+
+            foreach (Animal a in zoo)
+            {
+                a.Describe();
+                a.MakeSound();
+            }
+        }
+    }
+}
