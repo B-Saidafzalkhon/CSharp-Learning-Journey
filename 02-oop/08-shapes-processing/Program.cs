513331a325aa7d3182d77203e5d1f044960e7601
Saidafzalkhon B. <287926457+B-Saidafzalkhon@users.noreply.github.com>
2026-06-11 16:47:06 +0500

    Add "08-shapes-processing": Implement polymorphic shape processing

---
+++ 02-oop/08-shapes-processing/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace _08_shapes_processing
+{
+    internal class Program
+    {
+        public abstract class Shape
+        {
+            public abstract double Area();
+        }
+
+        public class Circle : Shape
+        {
+            public double Radius { get; private set; }
+            public Circle(double radius)
+            {
+                this.Radius = radius;
+            }
+            public override double Area() => Math.PI * (Radius * Radius);
+            public double Diameter() => 2 * Radius;
+
+            public override string ToString()
+            {
+                return $"Circle Area: {Area():F2}";
+            }
+        }
+        public class Rectangle : Shape
+        {
+            public double Width { get; private set; }
+            public double Height { get; private set; }
+            public Rectangle(double width, double height)
+            {
+                this.Width = width;
+                this.Height = height;
+            }
+            public override double Area() => Width * Height;
+
+            public override string ToString()
+            {
+                return $"Rectangle Area: {Area():F2}";
+            }
+        }
+
+        public class Triangle : Shape
+        {
+            public double TriangleBase { get; private set; }
+            public double Height { get; private set; }
+
+            public Triangle(double triangleBase, double height)
+            {
+                this.TriangleBase = triangleBase;
+                this.Height = height;
+            }
+
+            public override double Area() => 0.5 * TriangleBase * Height;
+            public override string ToString()
+            {
+                return $"Triangle Area: {Area():F2}";
+            }
+        }
+        static void Main(string[] args)
+        {
+            List<Shape> shapes = new()
+            {
+                new Rectangle(15,10),
+                new Triangle(10,10),
+                new Circle(15),
+                new Circle(10),
+                new Rectangle(10,15)
+            };
+
+            double totalArea = 0;
+            foreach (Shape shape in shapes)
+            {
+                totalArea += shape.Area();
+
+                Console.WriteLine(shape);
+                if (shape is Circle circle)
+                {
+                    double diameter = circle.Diameter();
+                    Console.WriteLine($"Circle Radius: {circle.Radius:F2}");
+                    Console.WriteLine($"Circle Diameter: {circle.Diameter():F2}");
+                }
+            }
+            Console.WriteLine($"\nTotal area: {totalArea:F2}");
+        }
+    }
+}
