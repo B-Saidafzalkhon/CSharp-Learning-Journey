97bb3d396894b683261d5de9ce11a3bf0d6651ef
Saidafzalkhon B. <287926457+B-Saidafzalkhon@users.noreply.github.com>
2026-06-12 22:16:31 +0500

    Add "11-composition": composition example with car and engine relationship

---
+++ 02-oop/11-composition/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _11_composition
+{
+    internal class Program
+    {
+        public class Car
+        {
+            public class Engine
+            {
+                public double Power { get; set; }
+
+                public Engine(double power)
+                {
+                    Power = power;
+                }
+                public void Start() => Console.WriteLine($"Engine power: {Power}hp | Car is started...");
+            }
+            // Car : Engine would be wrong because a car is not a type of engine.
+            // This would not be an appropriate "is-a" relationship.
+            // A car has an engine, so composition is the better choice here.
+            public string Name { get; set; }
+            public Engine CarEngine { get; set; }
+            public Car(string name, double power)
+            {
+                Name = name;
+                CarEngine = new Engine(power);
+            }
+
+            public void Start()
+            {
+                Console.WriteLine($"{Name} is starting...");
+                CarEngine.Start();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Car car = new Car("Nissan", 2000);
+            car.Start();
+        }
+    }
+}
