c55fea413f94ec9092e0f38d46fd0ddfc54332cc
Saidafzalkhon B. <287926457+B-Saidafzalkhon@users.noreply.github.com>
2026-06-12 22:22:48 +0500

    Fix: "11-composition" structure fixed

--- 02-oop/11-composition/Program.cs
+++ 02-oop/11-composition/Program.cs
@@ -2,18 +2,18 @@
 {
     internal class Program
     {
+        public class Engine
+        {
+            public double Power { get; set; }
+
+            public Engine(double power)
+            {
+                Power = power;
+            }
+            public void Start() => Console.WriteLine($"Engine power: {Power}hp | Car is started...");
+        }
         public class Car
         {
-            public class Engine
-            {
-                public double Power { get; set; }
-
-                public Engine(double power)
-                {
-                    Power = power;
-                }
-                public void Start() => Console.WriteLine($"Engine power: {Power}hp | Car is started...");
-            }
             // Car : Engine would be wrong because a car is not a type of engine.
             // This would not be an appropriate "is-a" relationship.
             // A car has an engine, so composition is the better choice here.
