8163a8316de643903032e8f60f3a48bb7df505aa
Saidafzalkhon B. <287926457+B-Saidafzalkhon@users.noreply.github.com>
2026-06-17 21:22:30 +0500

    Add generic Box class for storing typed values

---
+++ 03-advanced-csharp/06-generic-box/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _06_generic_box
+{
+    internal class Program
+    {
+        public class Box<T>
+        {
+            private T _value;
+            
+            public Box(T value)
+            {
+                _value = value;
+            }
+
+            public void Put(T item) { _value = item; }
+            public T GetItem() { return _value; }
+        }
+        static void Main(string[] args)
+        {
+            Box<int> intBox = new Box<int>(10);
+            Box<string> stringBox = new Box<string>("Apple");
+            Box<double> doubleBox = new Box<double>(3.14);
+
+            Console.WriteLine(intBox.GetItem());
+            Console.WriteLine(stringBox.GetItem());
+            Console.WriteLine(doubleBox.GetItem());
+
+            intBox.Put(25);
+            Console.WriteLine(intBox.GetItem());
+        }
+    }
+}
